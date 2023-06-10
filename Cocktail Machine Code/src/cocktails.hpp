0335d2ebee1449df8c379fa0e0e4ebb137906dd0
Hayden Evans <hev23@uclive.ac.nz>
2023-06-10 14:08:24 +1200

    Cocktail class header file

---
+++ Cocktail Machine Code/src/cocktails.hpp
@@ -0,0 +1,62 @@
+#ifndef COCKTAILS_H
+#define COCKTAILS_H
+
+enum Mixer {
+    None,
+    Tonic,
+    Lemonade,
+    Coke,
+    OrangeJ,
+    Prosseco,
+    PeachSchapps,
+    RasberryCordial
+};
+
+char* Mixers[] = {
+    "None",
+    "Tonic",
+    "Lemonade",
+    "Coke",
+    "OrangeJ",
+    "Prosseco",
+    "PeachSchapps",
+    "RasberryCordial"
+};
+
+enum Liqour {
+    None,
+    Gin,
+    Vodka,
+    Whiskey,
+    Tequila,
+    Rum
+};
+
+char* Liquors[] = {
+    "None",
+    "Gin",
+    "Vodka",
+    "Whiskey",
+    "Tequila",
+    "Rum"
+};
+
+
+
+class CocktailHandling {
+    public:
+    // Allows the code to continue running and periodically check if current < startTime + duration
+    void PumpOnTime(int pump, long int startTime, long int duration);
+    bool getStatus(int pump);
+
+    
+};
+
+
+class Cocktails {
+    public: 
+    // Initialise the cocktails ingredients when the class is created
+    Cocktails(Liqour liqour, Mixer mixerOne, Mixer mixerTwo, Mixer mixerThree, Mixer MixerFour);
+};
+
+#endif  

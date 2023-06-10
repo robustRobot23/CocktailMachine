0e1f12e06028a6f5cda4e236d206c75741c74a02
Hayden Evans <hev23@uclive.ac.nz>
2023-06-10 18:29:03 +1200

    Added pump class to turn pumps on and off. Each pump will need to be told the mL of the selected cocktail. The pumpRun method currently needs to be called frequently when pouring.

---
+++ Cocktail Machine Code/src/pumps.hpp
@@ -0,0 +1,31 @@
+#ifndef PUMPS_H
+#define PUMPS_H
+
+#include <Arduino.h>
+#include "pinDefinitions.hpp"
+#include "ingredients.hpp"
+
+#define MILLISECONDS_PER_MILLILITER 100
+
+class Pumps {
+    public:
+    Pumps(int p);
+    void initPump();
+    void resetPump();
+    void pumpRun();
+
+    Mixer mixer;
+    int milliLitre = 0;
+    bool finished = true;
+
+    private:
+    int pin;
+
+    long int startTime = 0;
+    long int pumpDuration = 0;
+
+}
+
+
+
+#endif

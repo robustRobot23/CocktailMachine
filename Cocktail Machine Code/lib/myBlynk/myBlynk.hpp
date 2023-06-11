e53b4edc27aba62935a21c1849325b0bb6559072
Hayden Evans <hev23@uclive.ac.nz>
2023-06-12 11:07:16 +1200

    Adding Blynk functionality temporarlily

---
+++ Cocktail Machine Code/lib/myBlynk/myBlynk.hpp
@@ -0,0 +1,18 @@
+#ifndef MY_BLYNK_H
+#define MY_BLYNK_H
+
+#include <Arduino.h>
+#include <Blynk.h>
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <privateInfo.hpp>
+
+
+volatile int blynkCocktail;
+volatile bool blynkCocktailSelected;
+
+
+
+#endif
